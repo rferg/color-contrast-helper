6c91457805ebfbb0539b2222aa66a1a0a52ef579
rferg <rmferguson77@gmail.com>
2020-04-30 16:15:00 -0400

    remove hsl color picker components for now

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,16 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ColorInputComponent } from './color-input/color-input.component';
 import { PopUpComponent } from './pop-up/pop-up.component';
-import { HueSliderComponent } from './hue-slider/hue-slider.component';
-import { SaturationLightnessFieldComponent } from './saturation-lightness-field/saturation-lightness-field.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     ColorInputComponent,
-    PopUpComponent,
-    HueSliderComponent,
-    SaturationLightnessFieldComponent
+    PopUpComponent
   ],
   imports: [
     CommonModule,
