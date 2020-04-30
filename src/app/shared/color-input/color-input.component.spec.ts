8d06f49d915666a2b9915d082af478fdfe0ec059
rferg <rmferguson77@gmail.com>
2020-04-30 18:14:25 -0400

    (WIP) added some tests for color-input.component

--- src/app/shared/color-input/color-input.component.spec.ts
+++ src/app/shared/color-input/color-input.component.spec.ts
@@ -4,6 +4,7 @@ import { of } from 'rxjs';
 import { ColorInputComponent } from './color-input.component';
 import { PopUpStubComponent } from 'src/testing/pop-up-stub-component';
 import { DocumentClickService } from 'src/app/core/services/document-click.service';
+import { Color } from 'src/app/core/models/color';
 
 describe('ColorInputComponent', () => {
   let component: ColorInputComponent;
@@ -34,5 +35,69 @@ describe('ColorInputComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('#getter:value', () => {
+    it('should return the value', () => {
+      const color = new Color('#abc123');
+      component.value = color;
+
+      expect(component.value).toEqual(color);
+    });
+  });
+
+  describe('#setter:value', () => {
+    it('should set value', () => {
+      const color = new Color('#abc123');
+      component.value = color;
+
+      expect(component.value).toEqual(color);
+    });
+
+    it('should call registered onChange callback if value changed', () => {
+      const onChangeSpy = jasmine.createSpy('onChange');
+      component.registerOnChange(onChangeSpy);
+
+      component.value = new Color('#abc123');
+
+      expect(onChangeSpy).toHaveBeenCalled();
+    });
+
+    it('should not call registered onChange callback if value is not changed', () => {
+      const color = new Color('#abc123');
+      component.value = color;
+      const onChangeSpy = jasmine.createSpy('onChange');
+      component.registerOnChange(onChangeSpy);
+
+      component.value = color;
+
+      expect(onChangeSpy).not.toHaveBeenCalled();
+    });
+
+    it('should should emit true to isDarkColorText$ if value changed and color luminance is above threshold',
+      (done: DoneFn) => {
+        component.darkTextLuminanceThreshold = 0;
+        const color = new Color('#abc123');
+
+        component.isDarkColorText$.subscribe(isDarkColorText => {
+          expect(isDarkColorText).toBe(true);
+          done();
+        });
+
+        component.value = color;
+    });
+
+    it('should should emit false to isDarkColorText$ if value changed and color luminance is below threshold',
+      (done: DoneFn) => {
+        component.darkTextLuminanceThreshold = 1;
+        const color = new Color('#abc123');
+
+        component.isDarkColorText$.subscribe(isDarkColorText => {
+          expect(isDarkColorText).toBe(false);
+          done();
+        });
+
+        component.value = color;
+    });
   });
 });
--- src/app/shared/color-input/color-input.component.ts
+++ src/app/shared/color-input/color-input.component.ts
@@ -1,10 +1,13 @@
-import { Component, ChangeDetectionStrategy, forwardRef, OnDestroy, ElementRef, OnInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy, forwardRef, OnDestroy, ElementRef, OnInit, Input } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { Color } from 'src/app/core/models/color';
 import { Subject, Observable, Subscription } from 'rxjs';
 import { Rgb } from 'src/app/core/models/rgb';
 import { DocumentClickService } from 'src/app/core/services/document-click.service';
 
+/**
+ * Form control component for {@link Color} values.
+ */
 @Component({
   selector: 'app-color-input',
   templateUrl: './color-input.component.html',
@@ -17,15 +20,30 @@ import { DocumentClickService } from 'src/app/core/services/document-click.servi
   }]
 })
 export class ColorInputComponent implements ControlValueAccessor, OnInit, OnDestroy {
+  /**
+   * The relative luminance of the value color, above which a dark color is used
+   * for the input text.
+   * Should be between 0 and 1.
+   * Default value is 1/3.
+   */
+  @Input() darkTextLuminanceThreshold = 1 / 3;
   private _value: Color;
+  /**
+   * Gets the color value for this input.
+   */
   get value(): Color {
     return this._value;
   }
+  /**
+   * Sets the color value for this input
+   */
   set value(color: Color) {
     if (color !== this._value) {
       this._value = color;
       if (this.onChange) { this.onChange(this._value); }
-      if (this._value) { this.isDarkColorTextSource.next(this._value.relativeLuminance > 0.333); }
+      if (this._value) {
+        this.isDarkColorTextSource.next(this._value.relativeLuminance > (this.darkTextLuminanceThreshold || 0));
+      }
     }
   }
   private onChange: (v: Color) => void;
@@ -34,12 +52,21 @@ export class ColorInputComponent implements ControlValueAccessor, OnInit, OnDest
   private documentClickSubscription: Subscription;
 
   private isDisabledSource = new Subject<boolean>();
+  /**
+   * Emits the disabled state of the input.
+   */
   isDisabled$: Observable<boolean> = this.isDisabledSource.asObservable();
 
   private isEditingSource = new Subject<boolean>();
+  /**
+   * Emits whether input value is being edited.
+   */
   isEditing$: Observable<boolean> = this.isEditingSource.asObservable();
 
   private isDarkColorTextSource = new Subject<boolean>();
+  /**
+   * Emits whether the input text color should be dark.
+   */
   isDarkColorText$ = this.isDarkColorTextSource.asObservable();
 
   constructor(
@@ -59,26 +86,44 @@ export class ColorInputComponent implements ControlValueAccessor, OnInit, OnDest
     if (this.documentClickSubscription) { this.documentClickSubscription.unsubscribe(); }
   }
 
+  /**
+   * Sets the input to have the provided value.
+   * @param value the {@link Color} to set as the value.
+   */
   writeValue(value: Color): void {
     this.value = value;
   }
-
+  /**
+   * Registers a callback to be called when the value is changed.
+   * @param fn the callback to call when value is changed
+   */
   registerOnChange(fn: (v: Color) => void): void {
     this.onChange = fn;
   }
-
+  /**
+   * Registers a callback to be called when input is blurred.
+   * @param fn the callback to call when input is blurred.
+   */
   registerOnTouched(fn: (v: Color) => void): void {
     this.onTouched = fn;
   }
-
+  /**
+   * Sets disabled state of the input
+   * @param isDisabled whether the input should be disabled
+   */
   setDisabledState(isDisabled: boolean): void {
     this.isDisabledSource.next(isDisabled);
   }
-
+  /**
+   * Called when input is blurred
+   */
   onBlur(): void {
     if (this.onTouched) { this.onTouched(this.value); }
   }
-
+  /**
+   * Called when value of hex input element changes.
+   * @param hex the hex value it was changed to
+   */
   onHexChange(hex: string): void {
     try {
       const newColor = new Color(hex);
@@ -87,7 +132,11 @@ export class ColorInputComponent implements ControlValueAccessor, OnInit, OnDest
       console.error(error);
     }
   }
-
+  /**
+   * Called when value of one of the RGB input elements changes.
+   * @param value the value changed to
+   * @param component which of R, G, or B inputs changed
+   */
   onRgbChange(value: number, component: 'r'|'g'|'b'): void {
     const newRgbValues = {
       ...(this.value || new Color('#000')).rgb,
@@ -97,6 +146,10 @@ export class ColorInputComponent implements ControlValueAccessor, OnInit, OnDest
       Color.convertRgbToHex(new Rgb(newRgbValues)));
   }
 
+  /**
+   * Sets the editing state of the input.
+   * @param isEditing whether input is being edited
+   */
   setIsEditing(isEditing: boolean): void {
     this.isEditingSource.next(isEditing);
   }
