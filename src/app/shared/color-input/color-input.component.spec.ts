3d1207a9ee1756c467170099604e93e75ea09f65
Ryan Ferguson <rferguson@cmdi-osa.org>
2020-05-01 16:29:27 -0400

    add some more tests for color-input.component

--- src/app/shared/color-input/color-input.component.spec.ts
+++ src/app/shared/color-input/color-input.component.spec.ts
@@ -1,24 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { of } from 'rxjs';
+import { of, Subject, Subscription } from 'rxjs';
 
 import { ColorInputComponent } from './color-input.component';
 import { PopUpStubComponent } from 'src/testing/pop-up-stub-component';
 import { DocumentClickService } from 'src/app/core/services/document-click.service';
 import { Color } from 'src/app/core/models/color';
+import { nameof } from 'src/app/core/models/nameof';
+import { FormsModule } from '@angular/forms';
 
 describe('ColorInputComponent', () => {
   let component: ColorInputComponent;
   let fixture: ComponentFixture<ColorInputComponent>;
   let docClickService: jasmine.SpyObj<DocumentClickService>;
+  const clickSource = new Subject<MouseEvent>();
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ FormsModule ],
       declarations: [ ColorInputComponent, PopUpStubComponent ],
       providers: [
         {
           provide: DocumentClickService,
           useValue: jasmine.createSpyObj('DocumentClickService', [], {
-            click$: of(new Event('click'))
+            click$: clickSource.asObservable()
           })
         }
       ]
@@ -30,15 +34,29 @@ describe('ColorInputComponent', () => {
     fixture = TestBed.createComponent(ColorInputComponent);
     component = fixture.componentInstance;
     docClickService = TestBed.inject(DocumentClickService);
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
+  describe('#constructor', () => {
+    it(`should set default ${nameof<ColorInputComponent>('darkTextLuminanceThreshold')}`, () => {
+      expect(component.darkTextLuminanceThreshold).toBe(1 / 3);
+    });
+
+    it(`should define ${nameof<ColorInputComponent>('isDisabled$')}, ` +
+      `${nameof<ColorInputComponent>('isEditing$')}, and ` +
+      `${nameof<ColorInputComponent>('isDarkColorText$')} observables`,
+      () => {
+        expect(component.isDisabled$).toBeDefined('isDisabled$ not defined');
+        expect(component.isEditing$).toBeDefined('isEditing$ not defined');
+        expect(component.isDarkColorText$).toBeDefined('isDarkColorText$ not defined');
+    });
+  });
+
   describe('#getter:value', () => {
-    it('should return the value', () => {
+    it(`should return the value`, () => {
       const color = new Color('#abc123');
       component.value = color;
 
@@ -74,7 +92,7 @@ describe('ColorInputComponent', () => {
       expect(onChangeSpy).not.toHaveBeenCalled();
     });
 
-    it('should should emit true to isDarkColorText$ if value changed and color luminance is above threshold',
+    it(`should should emit true to ${nameof<ColorInputComponent>('isDarkColorText$')} if value changed and color luminance is above threshold`,
       (done: DoneFn) => {
         component.darkTextLuminanceThreshold = 0;
         const color = new Color('#abc123');
@@ -87,7 +105,7 @@ describe('ColorInputComponent', () => {
         component.value = color;
     });
 
-    it('should should emit false to isDarkColorText$ if value changed and color luminance is below threshold',
+    it(`should should emit false to ${nameof<ColorInputComponent>('isDarkColorText$')} if value changed and color luminance is below threshold`,
       (done: DoneFn) => {
         component.darkTextLuminanceThreshold = 1;
         const color = new Color('#abc123');
@@ -98,6 +116,70 @@ describe('ColorInputComponent', () => {
         });
 
         component.value = color;
+    });
+
+    it('should set when value is null', () => {
+      component.value = null;
+
+      expect(component.value).toBeNull();
+    });
+  });
+
+  describe('#ngOnInit', () => {
+    it(`it should subscribe to ${nameof<DocumentClickService>('click$')}`, () => {
+      const spy = spyOn(docClickService.click$, 'subscribe');
+
+      component.ngOnInit();
+
+      expect(spy).toHaveBeenCalled();
+    });
+
+    describe(`${nameof<DocumentClickService>('click$')} subscription`, () => {
+      it(`should emit false to ${nameof<ColorInputComponent>('isEditing$')} if element does not contain event target`,
+        (done: DoneFn) => {
+          fixture.detectChanges();
+          component.isEditing$.subscribe(isEditing => {
+            expect(isEditing).toBe(false);
+            done();
+          });
+
+          clickSource.next(new MouseEvent('click'));
+        }
+      );
+
+      it(`should not call ${nameof<ColorInputComponent>('setIsEditing')} if element contains event target`, () => {
+        const spy = spyOn(component, 'setIsEditing');
+        fixture.detectChanges();
+        clickSource.next({
+          ...new MouseEvent('click'),
+          target: (fixture.debugElement.nativeElement as HTMLElement).appendChild(document.createElement('div'))
+        });
+
+
+        expect(spy).not.toHaveBeenCalledWith(false);
+      });
+
+      it(`should not call ${nameof<ColorInputComponent>('setIsEditing')} if event is falsy`, () => {
+        const spy = spyOn(component, 'setIsEditing');
+        fixture.detectChanges();
+        clickSource.next(null);
+
+        fixture.detectChanges();
+
+        expect(spy).not.toHaveBeenCalledWith(false);
+      });
+    });
+  });
+
+  describe('#ngOnDestroy', () => {
+    it(`should unsubscribe from subscription to ${nameof<DocumentClickService>('click$')}`, () => {
+      const unsubscribeSpy = jasmine.createSpy();
+      spyOn(docClickService.click$, 'subscribe').and.returnValue(new Subscription(unsubscribeSpy));
+      fixture.detectChanges();
+
+      component.ngOnDestroy();
+
+      expect(unsubscribeSpy).toHaveBeenCalled();
     });
   });
 });
